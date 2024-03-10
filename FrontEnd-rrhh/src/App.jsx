a70b6d09a58a5993cfb8dc33c5441f91023a35c8
HernanGoldy <hernanmdq@gmail.com>
2024-03-10 07:47:18 -0300

    modifico enrutamiento de la app

--- FrontEnd-rrhh/src/App.jsx
+++ FrontEnd-rrhh/src/App.jsx
@@ -1,11 +1,16 @@
+import { BrowserRouter, Route, Routes } from 'react-router-dom';
 import ListadoEmpleados from './components/ListadoEmpleados';
-import Navegacion from './template/navegacion';
+import Navegacion from './template/Navegacion';
 
 function App() {
 	return (
 		<div className="container">
-			<Navegacion />
-			<ListadoEmpleados />
+			<BrowserRouter>
+			<Navegacion/>
+			<Routes>
+				<Route exact path='/' element={<ListadoEmpleados/>}/>
+			</Routes>
+			</BrowserRouter>
 		</div>
 	);
 }
