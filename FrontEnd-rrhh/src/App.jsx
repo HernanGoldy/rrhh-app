b4c33565c9442d185987efdb0f64115fb2ffb4ad
HernanGoldy <hernanmdq@gmail.com>
2024-03-10 10:55:39 -0300

    agrego formulario para agregar empleado

--- FrontEnd-rrhh/src/App.jsx
+++ FrontEnd-rrhh/src/App.jsx
@@ -1,6 +1,7 @@
 import { BrowserRouter, Route, Routes } from 'react-router-dom';
 import ListadoEmpleados from './components/ListadoEmpleados';
 import Navegacion from './template/Navegacion';
+import AgregarEmpleado from './components/AgregarEmpleado';
 
 function App() {
 	return (
@@ -9,6 +10,7 @@ function App() {
 			<Navegacion/>
 			<Routes>
 				<Route exact path='/' element={<ListadoEmpleados/>}/>
+				<Route exact path='/agregar' element={<AgregarEmpleado/>}/>
 			</Routes>
 			</BrowserRouter>
 		</div>
